cd769f8c99301fc1f581f5819983d20dc3e4813a
Elisson Araújo <ersa@cin.ufpe.br>
2022-08-28 15:14:19 -0300

    refactor(client): changes variables names and includes prints

--- client.py
+++ client.py
@@ -1,29 +1,31 @@
-from email import message
 import socket
-msgClient = open(input(),"rb")
-retorno = "resposta.pdf"
-#msgFromClient = "Hello UDP Server"
-bufferSize = 1024
-bytesToSend = msgClient.read(bufferSize)
-serverAddressPort   = ("127.0.0.1", 20001)
+
+serverAddress   = ("localhost", 20001)
+bufferSize      = 1024
+fileRec         = "fileRecvFromServer.pdf"
 
 # Create a UDP socket at client side
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+file = open(input("Digite o nome do arquivo que deseja enviar: "), "rb")
+bytesToSend = file.read(bufferSize)
+
 # Send to server using created UDP socket
+print("Enviando...")
 while(bytesToSend):
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-    bytesToSend = msgClient.read(bufferSize)
-    #msgServer = UDPClientSocket.recvfrom(bufferSize)
-    #msg = "Message from Server {}".format(msgServer[0])
-    print("Enviando...")
-msgClient.close()
-UDPClientSocket.sendto('\x18'.encode(),serverAddressPort)
+    UDPClientSocket.sendto(bytesToSend, serverAddress)
+    bytesToSend = file.read(bufferSize)
+    
+file.close()
+UDPClientSocket.sendto('\x18'.encode(), serverAddress)
 print("Envio realizado!")
-msg_retorno = open(retorno,'wb')
-file, add = UDPClientSocket.recvfrom(bufferSize)
-while(file != '\x18'.encode()):
-    msg_retorno.write(file)
-    file, add = UDPClientSocket.recvfrom(bufferSize)
-msg_retorno.close()
+
+file = open(fileRec,'wb')
+data, address = UDPClientSocket.recvfrom(bufferSize)
+
+while(data != '\x18'.encode()):
+    file.write(data)
+    data, address = UDPClientSocket.recvfrom(bufferSize)
+
+file.close()
 UDPClientSocket.close()
