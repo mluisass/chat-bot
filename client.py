db0c9e838e34e9bfe4aa0156e3202631aae57a94
Maria Luísa <mlss@cin.ufpe.br>
2022-10-10 20:14:19 -0300

    wip: threads to send and receive messages

--- client.py
+++ client.py
@@ -1,32 +1,52 @@
 from random import randint
 from utils import *
-
+import threading as th
 class Client:
     #inicializacao do cliente
     def __init__(self, port):
         self.port = port
         self.client_socket = UDP(False) # False => é cliente
-        self.sendMessage()
+        try:
+            lock = th.Lock() 
+            send_thread = th.Thread(target= self.sendMessage, args=[lock] )
+            rcv_thread = th.Thread(target= self.receiveMessage, args=[lock])
+            
+            send_thread.daemon =True 
+            rcv_thread.daemon= True 
+
+            send_thread.start()
+            rcv_thread.start()
+
+            send_thread.join()
+            rcv_thread.join()
+
+        except KeyboardInterrupt:
+            self.client_socket.close()
 
     #envia uma mensagem e recebe de volta
-    def sendMessage(self):
+    def sendMessage(self,lock):
         while(True):
-            try:
-                # lê mensagem do teclado e envia ao servidor
-                msg = input("Digite mensagem a ser enviada ao servidor com no máximo 1000 caracteres: ")
-                self.client_socket.rdt_send(msg, SERVER_ADDRESS)
-
-                # recebe de volta a mensagem enviada
-                data, _ = self.client_socket.rdt_rcv()
-                msg = eval(data.decode())
-
-                print("Mensagem enviada: " + msg['data'])
-            except KeyboardInterrupt:
-                self.client_socket.close()
-                break
-
+            # lê mensagem do teclado e envia ao servidor
+            msg = input("Digite mensagem a ser enviada ao servidor com no máximo 1000 caracteres: ")
+            lock.acquire()
+            self.client_socket.rdt_send(msg, SERVER_ADDRESS)
+            lock.release()  
+        
+    def receiveMessage(self, lock):
+        while(True):
+            lock.acquire()
+            data, _ = self.client_socket.rdt_rcv()
+            lock.release()
+            msg = eval(data.decode())
+            print(msg['data'])
 if __name__ == "__main__":
     port = randint(3000,3100)
     print(port)
     Client(port)
-    
+
+# Cliente:
+# Precisa sempre estar disponível para receber e mandar
+
+# Threads:
+# 1. Receber mensagem
+# 2. Enviar mensagem
--- server.py
+++ server.py
@@ -1,37 +1,63 @@
 from utils import *
-
+import threading as th
 class Server:
     #inicializacao do servidor
     def __init__(self):
         self.server_socket = UDP(True) # True => é servidor
         self.current_user = None
         self.last_data_received =None
-    
-        self.run()
+        self.users = {}
 
-    def run(self):
+        try:
+            lock = th.Lock()
+            send_thread = th.Thread(target= self.broadcast, args=[lock, ""] )
+            rcv_thread = th.Thread(target= self.run, args=[lock])
+            
+            send_thread.daemon =True 
+            rcv_thread.daemon= True 
 
+            send_thread.start()
+            rcv_thread.start()
+
+            send_thread.join()
+            rcv_thread.join()
+
+        except KeyboardInterrupt:
+            self.server_socket.close()
+        
+
+    def run(self, lock):
         #espera em loop infinito pelo contato de um cliente
         while (True):
             try:
                 # recebe uma mensagem de um dos clientes
+                lock.acquire()
                 data, client_address = self.server_socket.rdt_rcv()
-                print(data.decode())
-
-                self.last_data_received = data
-                self.current_user = client_address
-
-                msg = eval(self.last_data_received.decode())
+                lock.release()
+                msg = eval(data.decode())
                 msg = msg['data']
+                print(msg[:15])
+                if msg[:15] == "hi, meu nome eh":
+                    print("entrei")
+                    user_name = msg[16:]
+                    self.users[user_name] = client_address # salva o usuário
 
-                # devolve ao cliente a mensagem enviada por ele
-                self.server_socket.rdt_send(msg, self.current_user)
+                    message = user_name + " entrou na sala"
+                    self.broadcast(lock, message)
+            
+                
             except KeyboardInterrupt:
                 self.server_socket.close()
                 break
 
+    def broadcast(self, lock, msg):
+        lock.acquire()
+        for key in self.users:
+            self.server_socket.rdt_send(msg, self.users[key])
+        lock.release()
+
     
-if __name__ == "__main__":
+if __name__ == "__main__":  
     Server()
 
     
